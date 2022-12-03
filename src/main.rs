6d5620294133902b436141c8b0b697768b0b12d4
KyahWill <knownbutnotloved@gmail.com>
2022-12-03 08:41:30 +0800

    changed the parsing a little bit

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use walkdir::{WalkDir,DirEntry};
 use std::fs::File;
 use std::io::prelude::*;
 use scraper::{Html,Selector};
-use regex;
+//use regex;
 
 #[derive(Debug)]
 #[allow(dead_code)]
@@ -52,7 +52,7 @@ fn get_all_files() -> Vec<DirEntry> {
     println!("file length: {}",files.len());
     return files;
 }
-fn parse_file(input: &DirEntry) -> Jurisprudence{
+fn parse_file(input: &DirEntry) -> String{
    
     // Opens the file.    println!("File name{}",file.path().display());
     let path = input.path(); 
@@ -72,37 +72,69 @@ fn parse_file(input: &DirEntry) -> Jurisprudence{
     // Parse html
     let html = Html::parse_document(&s);
     
-    // let h2_selector = Selector::parse("h2").unwrap();
+    // h2 selector selects the division
+    let h2_selector = Selector::parse("h2")
+                                .expect("Error found here");
+    let h2_element = &mut html.select(&h2_selector)
+                                .next()
+                                .expect("Selecting h2 failed");
+    let h2_text = String::from(h2_element.inner_html());
+    println!("H2_element = {}", h2_text );
+    
+    
+    //h3 selector selects the plaintiff/apellant or their role, can be separated via commas.
     let h3_selector = Selector::parse("h3")
                                 .expect("Error Found here");
-    // let h2_element = &mut html.select(&h2_selector);
     let h3_element = &mut html.select(&h3_selector)
                                 .next()
                                 .expect("selecting the title failed");
-    
-    let main_selector = Selector::parse("div")
-                                .expect("error while parsing Div");
-    let main_element = &mut html.select(&main_selector)
+    let h3_text =  String::from(h3_element.inner_html());
+    let  h3_split = h3_text.split("<br><br>")
+                            .nth(0)
+                            .unwrap();
+    println!("Parties Involved:");
+    for word in h3_split.split(",") {
+        println!("{}",word.trim());
+    }
+
+    //h4 selector selects GR Number.
+    let h4_selector = Selector::parse("h4")
+                                .expect("Error Found here");
+    let h4_element = &mut html.select(&h4_selector)
                                 .next()
-                                .expect("Selecting Div Main Failed");
-    let juris = Jurisprudence {
-        id:         String::from(input.path().file_name().unwrap().to_string_lossy().into_owned()),
-        title:      String::from(h3_element.inner_html()),
-        body:       String::from(main_element.inner_html()) ,
-        citations:  vec![
-                        Citations::Other("Test Citations".to_string()),
-                        Citations::Articles("Test".to_string())
-                    ] ,
-        date:       String::from("Dec 12, 2000"),
-        };
-    return juris;
+                                .expect("selecting the title failed");
+    //let citation_vector = std::Vec<std::String>;
+    println!("H4_element = {}", String::from(h4_element.inner_html()).trim());
+
+
+    //find where the first <hr> is and then select all the text below it. 
+    
+    if s.contains("<hr") {
+        println!("Found Horizontal Line");
+    }
+    //let main_selector = Selector::parse("div")
+    //                            .expect("error while parsing Div");
+    //let main_element = &mut html.select(&main_selector)
+    //                            .next()
+    //                            .expect("Selecting Div Main Failed");
+    //let juris = Jurisprudence {
+    //    id:         String::from(input.path().file_name().unwrap().to_string_lossy().into_owned()),
+    //    title:      String::from(h3_element.inner_html()),
+    //    body:       String::from(main_element.inner_html()) ,
+    //    citations:  vec![
+    //                    Citations::Other("Test Citations".to_string()),
+    //                    Citations::Articles("Test".to_string())
+    //                ] ,
+    //   date:       String::from("Dec 12, 2000"),
+    //    };
+    //return juris;
 
 
     //
     
     //
     // 
-
+    return String::from("Test")
 }
 
 //fn create_json_object() {
@@ -119,10 +151,11 @@ fn main() {
     //         println!("{}",counter);
     //     }
     // }
-    let juris = parse_file(&files[1]);
-    println!("Title:{} \nid:{}\ncitations:{:?}\n",juris.title, juris.id, juris.citations, );
-    let split = juris.title.split("VS.");
-    for piece in split{
-        println!("{}",piece);
-    }
+    let _juris = parse_file(&files[1]);
+    
+    //println!("Title:{} \nid:{}\ncitations:{:?}\n",juris.title, juris.id, juris.citations, );
+    // let split = juris.title.split("VS.");
+    //for piece in split{
+    //    println!("{}",piece);
+    //}
 }
