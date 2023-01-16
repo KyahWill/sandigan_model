2dbccc8f6648a3def915868bc8b41bfd206a165b
KyahWill <knownbutnotloved@gmail.com>
2023-01-16 16:58:48 +0800

    I'm not sure what I added here

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use walkdir::{WalkDir,DirEntry};
 use csv::{Writer};
 mod commands;
-use std::fs::copy;
+
 fn get_all_files() -> Vec<DirEntry> {
     
     let mut files: Vec<DirEntry> = Vec::new();
@@ -15,40 +15,33 @@ fn get_all_files() -> Vec<DirEntry> {
     println!("file length: {}",files.len());
     return files;
 }
-
-fn move_all_files(files: Vec<DirEntry>) {
-    let mut juris_id = 1;
-    for file in files { 
-        let path = file.path();
-        let file_name = path
-        .file_name().unwrap().to_str().unwrap();
-        let destination = format!("temp_target/{}",file_name);
-                                                        // .unwrap()
-                                      
-                                                        // .to_str().to_string());
-        
-        copy(path,destination).unwrap();
-        // let juris = commands::get_juris_details(file, juris_id);
-        juris_id+=1;
-        if juris_id %500 == 0 {
-            println!("{}",juris_id)
-        }
-    }
-}
 #[allow(dead_code)]
+
 fn parse_all_files( files: Vec<DirEntry>) {
     let mut juris_wtr = Writer::from_path("juris.csv").unwrap() ;
-    juris_wtr.write_record(&["identifier","title","file_url","year","month","day","gr Number"]).unwrap();
-    let mut juris_id = 1;
-    for file in files { 
-        let juris = commands::get_juris_details(file, juris_id);
-        juris_id+=1;
-        juris_wtr.write_record(&[juris.id,juris.title, juris.url, juris.year, juris.month, juris.day, juris.gr_number]).unwrap();
+    juris_wtr.write_record(&["identifier","title","file_url","year","month","day","gr Number", "division", "case_code"]).unwrap();
+    
+    let mut citation_wtr = Writer::from_path("citation.csv").unwrap();
+    citation_wtr.write_record(&["juris_id","case_citations"]).unwrap();
+
+    for (juris_id ,file) in files.iter().enumerate()  { 
+
+        //
+        let html_input = commands::path_to_html_string(file.to_owned());
+        let juris = commands::get_juris_details(html_input, file.to_owned(),juris_id);
+        // println!("{}, {}", file.path().display().to_string(), juris.citations.len());       
+        //writes down the list of jurisprudence in excel
+        juris_wtr.write_record(&[juris.id,juris.title, juris.url, juris.year, juris.month, juris.day, juris.gr_number, juris.division, juris.case_code]).unwrap();
+        
+        // Write down the list of citations.
+        for citation in juris.citations {
+            citation_wtr.write_record(&[juris_id.to_string(), citation]).unwrap();
+        }
+
         if juris_id %500 == 0 {
             println!("{}",juris_id)
         }
     }
-
 }
 
 // fn parse_all_citations() {
@@ -58,6 +51,7 @@ fn parse_all_files( files: Vec<DirEntry>) {
 
 fn main() {
     let files: Vec<DirEntry> = get_all_files();
-    // parse_all_files(files);
-    move_all_files(files)
+    parse_all_files(files);
+
+    // move_all_files(files)
 }  
